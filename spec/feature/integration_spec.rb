0ae753c5d4e284ea156548f9e36fc632bd935f36
nninarao09 <ninarao09@gmail.com>
2021-09-23 18:56:11 -0500

    Added Author attribute and new tests

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,8 +5,20 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'jk rowling'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
+    expect(page).to have_content('jk rowling')
+  end
+
+  scenario 'invalid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 5
+    fill_in 'Author', with: 3
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('')
+    expect(page).to have_content('')
   end
 end
